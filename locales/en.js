1305a46cd42c81d4bffad4cbb91e9501c54cb80c
ilmar <ilmar.tyrk@gmail.com>
2019-02-08 16:21:33 +0200

    add translation key and value

---
+++ locales/en.js
@@ -0,0 +1,3 @@
+{
+    "ep_title_limit.info": "Your title is getting very long - we suggest you narrowing it down to 100 characters to keep it simple and focused. Document titles get truncated in most representations except the document view"
+}
