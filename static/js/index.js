5e94bc9d979d363348dc6f2209c621be56274788
ilmar <ilmar.tyrk@gmail.com>
2020-12-22 12:52:58 +0200

    Change to interval from edit event

--- static/js/index.js
+++ static/js/index.js
@@ -100,29 +100,14 @@ let doInsertTitleLimitMark = function () {
 exports.aceInitialized = (hook, context) => {
   const editorInfo = context.editorInfo;
   editorInfo.ace_doInsertTitleLimitMark = _(doInsertTitleLimitMark).bind(context);
+  setInterval(function () {
+    context.editorInfo.ace_callWithAce(function(ace){
+      var activeLine = ace.ace_caretLine();
+      if (activeLine === 0) {
+          ace.ace_doInsertTitleLimitMark();
+      }
+    },'insertTitleLimitMark' , true);
+  },1000);
 };
 
-exports.aceEditEvent = function (hook, context, cb) {
-  const cs = context.callstack;
-  const rep = context.rep;
-  if (rep.selEnd && rep.selEnd[0] === 0 && (['setBaseText', 'handleClick'].indexOf(cs.type) >=0 || (cs.type === 'handleKeyEvent' && cs.observedSelection))) {
-    const padOuter = $('iframe[name="ace_outer"]').contents().find('body');
-    const padInner = padOuter.find('iframe[name="ace_inner"]').contents();
-    if (!$('#editorloadingbox').is(':visible')) {
-        setTimeout(function () {
-          context.editorInfo.ace_callWithAce(function(ace){
-            var activeLine = ace.ace_caretLine();
-            if (activeLine === 0) {
-                ace.ace_doInsertTitleLimitMark();
-            }
-          },'insertTitleLimitMark' , true);
-        });
-    }
-
-    return cb();
-  } else {
-    return cb();
-  }
-
-}
 exports.aceEditorCSS = () => ['ep_title_limit/static/css/ep_title_limit.css'];
